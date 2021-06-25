ae0a0ea0207e5cad3bb8f8f9f17c412312dda4cb
Magnus Rødseth <magnus.rodseth@gmail.com>
2021-06-25 20:56:46 +0200

    add array exercise

--- arrays/array.py
+++ arrays/array.py
@@ -1,87 +1,3 @@
-# Code with Mosh: Data Structures and Algorithms
-
-## The boring stuff 💭
-
-### What is it ❓
-
-This repository serves as a collection of code snippets from "The Ultimate Data Structures and Algorithms", a course by
-Code with Mosh. An overview of his courses can be found found [here](https://codewithmosh.com/courses).
-
-### Developer Information 🙋🏼‍♂️
-
-Developed and written by Magnus Rødseth.
-
-## The Big O Notation
-
-### O(1)
-
-```python
-print("Hello, world!")  # O(1)
-print("Hello, world again!")
-# --> O(2) --> O(1) --> Constant time
-```
-
-### O(n)
-
-```python
-# ----- Example 1 -----
-print("Hello, world!")  # O(1)
-
-for i in range(10):  # O(n)
-    print(i)
-
-print("Bye, world!")  # O(1)
-
-# ==> O(2 + n) ==> O(n)
-
-# ----- Example 2 -----
-numbers = [1, 2, 3]
-names = ["John", "Adam", "Eve"]
-
-for num in numbers:  # O(n)
-    print(num)
-
-for name in names:  # O(m)
-    print(name)
-
-# ==> O(n + m) ==> O(n)
-# Still linear growth
-```
-
-### O(n^2)
-
-```python
-numbers = [1, 2, 3]
-
-for first in numbers:  # O(n)
-    for second in numbers:  # O(n)
-        print(first + second)
-# ==> O(n^2)
-
-for num in numbers:  # O(n)
-    print(num)
-# ==> O(n + n^2) ==> O(n^2)
-
-for first in numbers:  # O(n)
-    for second in numbers:  # O(n)
-        for third in numbers:  # O(n)
-            print(first + second + third)
-# ==> O(n^3)
-```
-
-### O(log n)
-
-**Insert content here after learning about graphs and trees.**
-
-### O(2^n)
-
-**Insert content here after learning more.**
-
-## Arrays
-
-### Exercise: Building an Array class
-
-```python
 from typing import List
 
 
@@ -192,4 +108,34 @@ class Array:
         copy.append(0)
 
         return copy
-```
+
+
+array = Array(3)
+
+array.print()
+
+array.insert(1)
+array.insert(2)
+array.insert(3)
+
+array.print()
+
+array.insert(4)
+
+array.print()
+
+value = array.remove_at(3)
+
+index = array.index_of(4)
+
+print(f'Index: {index}')
+
+print(f'Removed value: {value}')
+
+array.print()
+
+value = array.remove_at(1)
+
+print(f'Removed value: {value}')
+
+array.print()
